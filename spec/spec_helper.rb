e59852f084e957e6aff1d3d323ae9ff7e3f2b543
Boris Filipov <bf@ninjaconcept.com>
2014-03-12 08:18:20 +0100

    RSpec configuration.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,17 @@ require 'rspec'
 require 'webmock'
 require 'vcr'
 
+RSpec.configure do |c|
+   # Use color in STDOUT
+  c.color_enabled = true
+
+  # Use color not only in STDOUT but also in pagers and files
+  c.tty = true
+
+  # Use the specified formatter
+  c.formatter = :documentation # :progress, :html, :textmate
+end
+
 VCR.configure do |c|
   c.cassette_library_dir = "fixtures/vcr_cassettes"
   c.hook_into :webmock
