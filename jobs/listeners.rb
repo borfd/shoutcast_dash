146cf969066d1e32c73218177a9e8e18922ec38d
Boris Filipov <bf@ninjaconcept.com>
2014-03-12 08:10:25 +0100

    extracting shoutcast class.

--- jobs/listeners.rb
+++ jobs/listeners.rb
@@ -1,24 +1,5 @@
-require 'nokogiri'
-require 'open-uri'
-
-class Shoutcast
-    def noko
-      listeners_uri = open("http://dirtybass.fm:8000/admin.cgi?sid=1&mode=viewxml&page=3",
-        http_basic_authentication: ["admin", "dirtyisbetter"])
-      Nokogiri::HTML(listeners_uri)
-    end
-
-    def listeners
-      noko.css('listener').count
-    end
-
-    def user_agents
-      noko.css('listener')
-    end
-end
-
 listener_history = []
-(1..60).each do |i|
+(1..360).each do |i|
   listener_history << {x:i, y: 0}
 end
 last_x = listener_history.last[:x]
