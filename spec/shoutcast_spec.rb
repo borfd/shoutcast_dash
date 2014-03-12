ede4499af9ccf8e03ebdb963598c9f72c60529ac
Boris Filipov <bf@ninjaconcept.com>
2014-03-12 08:36:45 +0100

    stream response generator

--- spec/shoutcast_spec.rb
+++ spec/shoutcast_spec.rb
@@ -4,7 +4,7 @@ require_relative "../lib/shoutcast"
 describe Shoutcast do
   it "returns the listeners" do
     VCR.use_cassette('shoutcast') do
-      expect(Shoutcast.new.listener_count).to eq(4)
+      expect(Shoutcast.new.listener_count).to eq(5)
     end
   end
 end
