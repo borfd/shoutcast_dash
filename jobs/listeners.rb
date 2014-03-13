935d611a870e51d9b43cdb4ded828756817a446f
Boris Filipov <bf@ninjaconcept.com>
2014-03-13 21:59:55 +0100

    display unique listeners.

--- jobs/listeners.rb
+++ jobs/listeners.rb
@@ -2,19 +2,26 @@ listener_history = []
 (1..60*60).each do |i|
   listener_history << {x:i, y: 0}
 end
+
 last_x = listener_history.last[:x]
 
 tracker = ListenerTracker.new
 
+start_time = Time.now
+
 SCHEDULER.every '1s' do
+  shoutcast = Shoutcast.new
+
   listener_history.shift
   last_x += 1
-  shoutcast = Shoutcast.new
   listener_history << { x: last_x, y: shoutcast.listener_count }
 
   send_event('listener_graph', points: listener_history)
 
   send_event('listener_user_agents', items: shoutcast.user_agents)
+
+  tracker.post_listeners shoutcast.ips
+  send_event('unique_listeners', { current: tracker.unique_listeners, last: 0 })
 end
 
 SCHEDULER.every '1m' do
